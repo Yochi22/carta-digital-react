674babcef031d9e454ce7f0306b8b71797c4b2a4
Yochi22 <yochhii@gmail.com>
2023-10-24 20:02:54 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import 'semantic-ui-css/semantic.min.css'
+import "./scss/global.scss"
+import "react-toastify/dist/ReactToastify.css"
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+
+);
+
+reportWebVitals();
